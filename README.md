244ce0cf492196ae4262a5bf93f4536e639bd0f9
Philipp Kursawe <phil.kursawe@gmail.com>
2023-10-20 01:04:16 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# brickmerge® Preise
+
+GreaseMonkey UserScript welches den Bestpreis von brickmerge® zu folgenden Seiten hinzufügt:
+
+amazon.de
+mytoys.de
+smythstoys.com
+toys-for-fun.com
+
+## License
+
+MIT
---
+++ brickmerge.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name           brickmerge® Prices
+// @name:de        brickmerge® Preise
+// @namespace      https://brickmerge.de/
+// @version        1.0
+// @license        MIT
+// @description    Displays lowest brickmerge® price next to offer price
+// @description:de Zeigt den bisherigen Bestpreis von brickmerge® parallel zum aktuellen Preis an
+// @author         Philipp Kursawe <pke@pke.fyi>
+// @match          https://www.amazon.de/LEGO-*
+// @match          https://www.mytoys.de/lego-*
+// @match          https://www.smythstoys.com/de/de-de/spielzeug/lego/*
+// @match          https://www.toys-for-fun.com/de/lego*
+// @icon           https://www.google.com/s2/favicons?sz=64&domain=brickmerge.de
+// @supportURL     https://github.com/pke/brickmerge-userscript/discussions
+// @grant          GM_log
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const resolvers = {
+        "www.amazon.de": {
+            setSelector: "#productTitle",
+            targetSelector: "#corePriceDisplay_desktop_feature_div,#corePrice_feature_div",
+        },
+        "www.mytoys.de": {
+            setSelector: ".prod-info__name",
+            targetSelector: ".prod-info__price-container",
+        },
+        "www.smythstoys.com": {
+            setSelector: "h1[itemprop=name]",
+            targetSelector: "#product-info div[itemprop=price]",
+        },
+        "www.toys-for-fun.com": {
+            setSelector: "h1.page-title span[itemprop=name]",
+            targetSelector: ".product-info-price",
+        },
+    };
+
+    function renderError(element, error) {
+        if (!element) {
+            return;
+        }
+        const errorElement = document.createElement("div");
+        errorElement.innerText = error;
+        element.append(errorElement);
+    }
+
+    function addLowestPrice(element, setNumber, lowestPrice) {
+        if (!element) {
+            return;
+        }
+        const brickmergeBox = document.createElement("div");
+        const brickmergeLink = `<a href="https://brickmerge.de/${setNumber}">${lowestPrice}</a>`;
+        brickmergeBox.innerHTML = `<span>brickmerge Bestpreis: ${brickmergeLink}</span>`;
+        element.append(brickmergeBox);
+    }
+
+    const resolver = resolvers[document.location.host]
+    if (!resolver) {
+        return;
+    }
+
+    // Fetch the LEGO set number from the title
+    const title = document.querySelector(resolver.setSelector)?.textContent;
+    GM_log("title: ", title);
+    const [, setNumber] = /(\d+)/.exec(title) || [];
+    GM_log("set number: ", setNumber);
+    if (setNumber) {
+        fetch(`https://www.brickmerge.de/_app.php?find=${setNumber}&json_token=zNrPtJiFeOoOLpDjAMctsNzOrvi8KipF`)
+        .then(res => res.json(), () => ({ error: "brickmerge® nicht erreichbar" }))
+        .then(({ offers, error }) => {
+            GM_log("offers", offers);
+            if (!offers && !error) {
+                return;
+            }
+            const targets = document.querySelectorAll(resolver.targetSelector)
+            if (error) {
+                for (let element of targets) {
+                    renderError(element, error);
+                }
+            } else {
+                const lowestPrice = Number(offers?.lowPrice).toLocaleString("de-DE", {style: "currency", currency: offers?.priceCurrency || "EUR" });
+                if (lowestPrice) {
+                    for (let element of targets) {
+                        //GM_log("target:", element.innerHTML);
+                        addLowestPrice(element, setNumber, lowestPrice);
+                    }
+                }
+            }
+        });
+    }
+})();
