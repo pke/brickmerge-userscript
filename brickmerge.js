4f8b32cbc886d4d3ac326cc06b3fa7b3c8346997
Philipp Kursawe <phil.kursawe@gmail.com>
2023-11-09 23:16:07 +0100

    feat(Shop): add bol.de (same as thalia.de)

--- brickmerge.js
+++ brickmerge.js
@@ -13,6 +13,7 @@
 // @match          https://www.amazon.de/*LEGO*
 // @match          https://www.amazon.de/dp/*
 // @match          https://www.amazon.de/*/dp/*
+// @match          https://www.bol.de/shop/home/artikeldetails/*
 // @match          https://www.ebay.de/itm/*
 // @match          https://www.jb-spielwaren.de/*
 // @match          https://www.kleinanzeigen.de/s-anzeige/lego-*
@@ -32,214 +33,215 @@
 // ==/UserScript==
 
 (function() {
-  'use strict';
+    'use strict';
 
-   const style = `
-     .brickmerge-price {
-       background-color: #b00 !important;
-       color: #fff !important;
-       margin: 1rem 0 !important;
-       padding: 0.3rem 0.5rem !important;
-     }
-     .brickmerge-price a {
-       color: #fff !important;
-       font-weight: bold !important;
-       text-decoration: underline !important;
-     }
-     .brickmerge-price a:hover {
-       text-decoration: none !important;
-     }
-     .brickmerge-price img {
-       height: 16px;
-       display: inline;
-       vertical-align: middle;
-       margin-right: 0.3rem;
-     }
-     .brickmerge-price img.small {
-       width: 16px;
-     }
-     `;
-  const logo = `https://raw.githubusercontent.com/pke/brickmerge-userscript/master/public/images/brickmerge.svg`;
+     const style = `
+       .brickmerge-price {
+         background-color: #b00 !important;
+         color: #fff !important;
+         margin: 1rem 0 !important;
+         padding: 0.3rem 0.5rem !important;
+       }
+       .brickmerge-price a {
+         color: #fff !important;
+         font-weight: bold !important;
+         text-decoration: underline !important;
+       }
+       .brickmerge-price a:hover {
+         text-decoration: none !important;
+       }
+       .brickmerge-price img {
+         height: 16px;
+         display: inline;
+         vertical-align: middle;
+         margin-right: 0.3rem;
+       }
+       .brickmerge-price img.small {
+         width: 16px;
+       }
+       `;
+    const logo = `https://raw.githubusercontent.com/pke/brickmerge-userscript/master/public/images/brickmerge.svg`;
 
-  const resolvers = {
-      "www.amazon.de": {
-          targetSelector: "#corePriceDisplay_desktop_feature_div,#corePrice_feature_div",
-          testURL: "https://www.amazon.de/LEGO-43230-Disney-Kamera-Maus-Minifiguren/dp/B0BV7BMPVS",
-      },
-      "www.mytoys.de": {
-          targetSelector: ".prod-info__price-container",
-          testURL: "https://www.mytoys.de/lego-lego-disney-43230-kamera--hommage-an-walt-disney-29981540.html",
-      },
-      "www.smythstoys.com": {
-          targetSelector: "#product-info div[itemprop=price]",
-          testURL: "https://www.smythstoys.com/de/de-de/spielzeug/lego/lego-fuer-erwachsene/lego-icons-set-10266-nasa-apollo-11-mondlandefaehre/p/183613",
-      },
-      "www.toys-for-fun.com": {
-          targetSelector: ".product-info-price",
-          testURL: "https://www.toys-for-fun.com/de/legor-disney-43230-kamera-hommage-an-walt-disney.html",
-      },
-      "www.jb-spielwaren.de": {
-          targetSelector: ".widget-availability",
-          testURL: "https://www.jb-spielwaren.de/lego-10293-besuch-des-weihnachtsmanns/a-10293/",
-      },
-      "steinehelden.de": {
-          articleExtractor: /(\d+)/,
-          targetSelector: "div[itemprop=offers] .product--tax",
-          testURL: "https://steinehelden.de/city-arktis-schneemobil-60376/",
-      },
-      "www.proshop.de": {
-          targetSelector: "#site-product-price-stock-buy-container span.site-currency-wrapper",
-          testURL: "https://www.proshop.de/LEGO/LEGO-Ideas-21343-Wikingerdorf/3195765",
-      },
-      "www.alternate.de": {
-        targetSelector: "#product-top-right .vat-and-shipping-costs",
-        testURL: "https://www.alternate.de/LEGO/10311-Creator-Expert-Orchidee-Konstruktionsspielzeug/html/product/1818749",
-      },
-      "www.saturn.de": {
-        targetSelector: "div[data-test='mms-pdp-offer-selection']",
-        prepend: true,
-        dynamic: true, // Site changes its DOM via script and could remove our element
-        styleSelector: "div[data-test='mms-branded-price'] p > span",
-        style(element) {
-            element.style = "text-align: right";
+    const resolvers = {
+        "www.amazon.de": {
+            targetSelector: "#corePriceDisplay_desktop_feature_div,#corePrice_feature_div",
+            testURL: "https://www.amazon.de/LEGO-43230-Disney-Kamera-Maus-Minifiguren/dp/B0BV7BMPVS",
         },
-        testURL: "https://www.saturn.de/de/product/_lego-10281-bonsai-baum-2672008.html",
-      },
-      "www.mediamarkt.de": "www.saturn.de", // just an alias, same as saturn
-      "www.otto.de": {
-        targetSelector: ".pdp_price__inner",
-        prepend: true,
-        testURL: "https://www.otto.de/p/lego-konstruktionsspielsteine-kamera-hommage-an-walt-disney-43230-lego-disney-811-st-made-in-europe-C1725197870/#variationId=1725014125",
-      },
-      "www.mueller.de": {
-        targetSelector: ".mu-product-price.mu-product-details-page__price",
-        testURL: "https://www.mueller.de/p/lego-icons-10281-bonsai-baum-kunstpflanzen-set-fuer-erwachsene-deko-2681620/",
-      },
-      "www.thalia.de": {
-        targetSelector: "artikel-informationen",
-        style(element) {
-            element.classList.add("element-text-small");
+        "www.mytoys.de": {
+            targetSelector: ".prod-info__price-container",
+            testURL: "https://www.mytoys.de/lego-lego-disney-43230-kamera--hommage-an-walt-disney-29981540.html",
         },
-        testURL: "https://www.thalia.de/shop/home/artikeldetails/A1068002914",
-      },
-      "www.ebay.de": {
-          parent: true,
+        "www.smythstoys.com": {
+            targetSelector: "#product-info div[itemprop=price]",
+            testURL: "https://www.smythstoys.com/de/de-de/spielzeug/lego/lego-fuer-erwachsene/lego-icons-set-10266-nasa-apollo-11-mondlandefaehre/p/183613",
+        },
+        "www.toys-for-fun.com": {
+            targetSelector: ".product-info-price",
+            testURL: "https://www.toys-for-fun.com/de/legor-disney-43230-kamera-hommage-an-walt-disney.html",
+        },
+        "www.jb-spielwaren.de": {
+            targetSelector: ".widget-availability",
+            testURL: "https://www.jb-spielwaren.de/lego-10293-besuch-des-weihnachtsmanns/a-10293/",
+        },
+        "steinehelden.de": {
+            articleExtractor: /(\d+)/,
+            targetSelector: "div[itemprop=offers] .product--tax",
+            testURL: "https://steinehelden.de/city-arktis-schneemobil-60376/",
+        },
+        "www.proshop.de": {
+            targetSelector: "#site-product-price-stock-buy-container span.site-currency-wrapper",
+            testURL: "https://www.proshop.de/LEGO/LEGO-Ideas-21343-Wikingerdorf/3195765",
+        },
+        "www.alternate.de": {
+          targetSelector: "#product-top-right .vat-and-shipping-costs",
+          testURL: "https://www.alternate.de/LEGO/10311-Creator-Expert-Orchidee-Konstruktionsspielzeug/html/product/1818749",
+        },
+        "www.saturn.de": {
+          targetSelector: "div[data-test='mms-pdp-offer-selection']",
           prepend: true,
-          style: "text-align: center",
-          targetSelector: ".x-bin-price",
-          testURL: "https://www.ebay.de/itm/204515604952",
-      },
-      "www.alza.de": {
-          parent: true,
+          dynamic: true, // Site changes its DOM via script and could remove our element
+          styleSelector: "div[data-test='mms-branded-price'] p > span",
+          style(element) {
+              element.style = "text-align: right";
+          },
+          testURL: "https://www.saturn.de/de/product/_lego-10281-bonsai-baum-2672008.html",
+        },
+        "www.mediamarkt.de": "www.saturn.de", // just an alias, same as saturn
+        "www.otto.de": {
+          targetSelector: ".pdp_price__inner",
           prepend: true,
-          targetSelector: ".price-detail__row",
-          testURL: "https://www.alza.de/spielzeug/lego-disney-43230-kamera-hommage-an-walt-disney-d7744520.htm",
-      },
-      "www.kleinanzeigen.de": {
-          parent: true,
-          prepend: true,
-          targetSelector: "#viewad-title,.ad-keydetails--price-and-shipping",
-      },
-  };
+          testURL: "https://www.otto.de/p/lego-konstruktionsspielsteine-kamera-hommage-an-walt-disney-43230-lego-disney-811-st-made-in-europe-C1725197870/#variationId=1725014125",
+        },
+        "www.mueller.de": {
+          targetSelector: ".mu-product-price.mu-product-details-page__price",
+          testURL: "https://www.mueller.de/p/lego-icons-10281-bonsai-baum-kunstpflanzen-set-fuer-erwachsene-deko-2681620/",
+        },
+        "www.thalia.de": {
+          targetSelector: "artikel-informationen",
+          style(element) {
+              element.classList.add("element-text-small");
+          },
+          testURL: "https://www.thalia.de/shop/home/artikeldetails/A1068002914",
+        },
+        "www.bol.de": "www.thalia.de", // https://www.bol.de/shop/home/artikeldetails/A1066075411
+        "www.ebay.de": {
+            parent: true,
+            prepend: true,
+            style: "text-align: center",
+            targetSelector: ".x-bin-price",
+            testURL: "https://www.ebay.de/itm/204515604952",
+        },
+        "www.alza.de": {
+            parent: true,
+            prepend: true,
+            targetSelector: ".price-detail__row",
+            testURL: "https://www.alza.de/spielzeug/lego-disney-43230-kamera-hommage-an-walt-disney-d7744520.htm",
+        },
+        "www.kleinanzeigen.de": {
+            parent: true,
+            prepend: true,
+            targetSelector: "#viewad-title,.ad-keydetails--price-and-shipping",
+        },
+    };
 
-  function renderError(element, error, operation = "append") {
-      if (!element) {
-          return;
-      }
-      const errorElement = document.createElement("div");
-      errorElement.innerText = error.message;
-      element[operation]?.(errorElement);
-  }
+    function renderError(element, error, operation = "append") {
+        if (!element) {
+            return;
+        }
+        const errorElement = document.createElement("div");
+        errorElement.innerText = error.message;
+        element[operation]?.(errorElement);
+    }
 
-  function addLowestPrice(element, title = "Bestpreis wird geladen", url, lowestPrice, operation = "append", icon, iconClass = []) {
-      if (!element) {
-          return;
-      }
-      let brickmergeBox = element.querySelector(".brickmerge-price");
-      let isNew = !brickmergeBox;
-      // console.log("addLowestPrice isNew: ", isNew);
-      if (!brickmergeBox) {
-          brickmergeBox = document.createElement("div");
-          brickmergeBox.className = "brickmerge-price";
-      }
-      const brickmergeLink = url ? `<a href="${url}">${lowestPrice}</a>` : "...";
-      const iconImage = icon ? `<img src="${icon}" class="${iconClass.join(" ")}"/>` : "";
-      brickmergeBox.innerHTML = `${iconImage}${title}: ${brickmergeLink}`;
-      if (isNew) {
-          element[operation]?.(brickmergeBox);
-      }
-      return brickmergeBox;
-  }
+    function addLowestPrice(element, title = "Bestpreis wird geladen", url, lowestPrice, operation = "append", icon, iconClass = []) {
+        if (!element) {
+            return;
+        }
+        let brickmergeBox = element.querySelector(".brickmerge-price");
+        let isNew = !brickmergeBox;
+        // console.log("addLowestPrice isNew: ", isNew);
+        if (!brickmergeBox) {
+            brickmergeBox = document.createElement("div");
+            brickmergeBox.className = "brickmerge-price";
+        }
+        const brickmergeLink = url ? `<a href="${url}">${lowestPrice}</a>` : "...";
+        const iconImage = icon ? `<img src="${icon}" class="${iconClass.join(" ")}"/>` : "";
+        brickmergeBox.innerHTML = `${iconImage}${title}: ${brickmergeLink}`;
+        if (isNew) {
+            element[operation]?.(brickmergeBox);
+        }
+        return brickmergeBox;
+    }
 
-  function addPriceToTargets(resolver, priceOrError, url, styleClasses, title, icon, iconClass) {
-      const targets = document.querySelectorAll(resolver.targetSelector);
-      if (targets.length === 0) {
-          return;
-      }
-      if (!document.querySelector("head style.brickmerge")) {
-          const styleElement = document.createElement("style");
-          styleElement.className = "brickmerge";
-          styleElement.type = 'text/css';
-          styleElement.innerHTML = style;
-          document.head.appendChild(styleElement);
-      }
-      const error = priceOrError instanceof Error && priceOrError
-      const price = error ? undefined : priceOrError
-      if (error instanceof Error) {
-          for (let element of targets) {
-              if (resolver.parent) {
-                  element = element.parentElement
-              }
-              renderError(element, error, resolver.prepend ? "prepend" : "append");
-          }
-      } else if (price) {
-          for (let element of targets) {
-              if (resolver.parent) {
-                  element = element.parentElement
-              }
-              //console.log("target:", element.innerHTML);
-              const box = addLowestPrice(element, title, url, price, resolver.prepend ? "prepend" : "append", icon, iconClass);
-              if (styleClasses) {
-                  box.classList.add(...styleClasses.split(" "));
-              }
-              if (typeof resolver.style === "function") {
-                  resolver.style(box);
-              } else if (typeof resolver.style === "string") {
-                  box.style = resolver.style;
-              }
-          }
-      }
-  }
+    function addPriceToTargets(resolver, priceOrError, url, styleClasses, title, icon, iconClass) {
+        const targets = document.querySelectorAll(resolver.targetSelector);
+        if (targets.length === 0) {
+            return;
+        }
+        if (!document.querySelector("head style.brickmerge")) {
+            const styleElement = document.createElement("style");
+            styleElement.className = "brickmerge";
+            styleElement.type = 'text/css';
+            styleElement.innerHTML = style;
+            document.head.appendChild(styleElement);
+        }
+        const error = priceOrError instanceof Error && priceOrError
+        const price = error ? undefined : priceOrError
+        if (error instanceof Error) {
+            for (let element of targets) {
+                if (resolver.parent) {
+                    element = element.parentElement
+                }
+                renderError(element, error, resolver.prepend ? "prepend" : "append");
+            }
+        } else if (price) {
+            for (let element of targets) {
+                if (resolver.parent) {
+                    element = element.parentElement
+                }
+                //console.log("target:", element.innerHTML);
+                const box = addLowestPrice(element, title, url, price, resolver.prepend ? "prepend" : "append", icon, iconClass);
+                if (styleClasses) {
+                    box.classList.add(...styleClasses.split(" "));
+                }
+                if (typeof resolver.style === "function") {
+                    resolver.style(box);
+                } else if (typeof resolver.style === "string") {
+                    box.style = resolver.style;
+                }
+            }
+        }
+    }
 
-  let resolver = resolvers[document.location.host]
-  // Do we have an alias for another resolver?
-  if (typeof resolver === "string") {
-      resolver = resolvers[resolver];
-  }
-  if (!resolver) {
-      return;
-  }
+    let resolver = resolvers[document.location.host]
+    // Do we have an alias for another resolver?
+    if (typeof resolver === "string") {
+        resolver = resolvers[resolver];
+    }
+    if (!resolver) {
+        return;
+    }
 
-  // Fetch the LEGO set number from the title
-  //console.log("title: ", document.title);
-  const [, setNumber] = (resolver.articleExtractor || /LEGO.*?(\d{4,})/i).exec(document.title) || [];
-  //console.log("set number: ", setNumber);
+    // Fetch the LEGO set number from the title
+    //console.log("title: ", document.title);
+    const [, setNumber] = (resolver.articleExtractor || /LEGO.*?(\d{4,})/i).exec(document.title) || [];
+    //console.log("set number: ", setNumber);
 
-  const styleNode = document.querySelector(resolver.styleSelector);
-  // console.log("styleNode", styleNode);
-  const styleClasses = styleNode?.className;
+    const styleNode = document.querySelector(resolver.styleSelector);
+    // console.log("styleNode", styleNode);
+    const styleClasses = styleNode?.className;
 
-  if (setNumber) {
-      if (!resolver.dynamic) {
-          addPriceToTargets(resolver, "...", "", styleClasses);
-      }
-      fetch("https://brickmerge-userscript.hypermedia.rocks/lowest/" + setNumber)
-      .then(res => res.json(), () => ({ error: "brickmerge® nicht erreichbar" }))
-      .then(json => {
-          const { title, links } = json;
-          const icon = links.find(link => link.rel == "icon") || { href: logo };
-          const link = links.find(link => link.rel == "self");
-          addPriceToTargets(resolver, link.title, link.href, styleClasses, title, icon.href, icon.class);
-      });
-  }
-})();
+    if (setNumber) {
+        if (!resolver.dynamic) {
+            addPriceToTargets(resolver, "...", "", styleClasses);
+        }
+        fetch("https://brickmerge-userscript.hypermedia.rocks/lowest/" + setNumber)
+        .then(res => res.json(), () => ({ error: "brickmerge® nicht erreichbar" }))
+        .then(json => {
+            const { title, links } = json;
+            const icon = links.find(link => link.rel == "icon") || { href: logo };
+            const link = links.find(link => link.rel == "self");
+            addPriceToTargets(resolver, link.title, link.href, styleClasses, title, icon.href, icon.class);
+        });
+    }
+  })();
